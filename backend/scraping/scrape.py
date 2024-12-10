52320f125bf58873cbbb2e88690e7713d1cfe015
Vitto28 <scoccv@usi.ch>
2024-12-10 21:18:12 +0100

    Modify output path in main scraper

--- backend/scraping/scrape.py
+++ backend/scraping/scrape.py
@@ -1,12 +1,34 @@
 import os
 import scrapy
 
-# Define spiders and output file paths
-spiders = {
-    "medlineplus": "../data/processed/medline_NEWEST.json",
-    # "drugs": "/path/to/output/drugs_data.json",
-    # "webmd": "/path/to/output/webmd_data.json",
-}
+from datetime import datetime
 
-for spider, output_file in spiders.items():
+
+def generate_id():
+    """
+    Generates a unique ID based on the current date and time up to seconds.
+
+    Format: YYYYMMDDHHMMSS
+
+    Returns:
+        str: A string representing the unique ID.
+    """
+    current_time = datetime.now()
+    unique_id = current_time.strftime("%Y%m%d%H%M%S")
+    return unique_id
+
+
+# Example usage
+print(generate_id())
+
+
+# Define spiders
+# spiders = ["medlineplus", "drugs", "webmd"]
+spiders = ["medlineplus"]
+
+output_base_path = os.path.join(os.path.dirname(__file__), "..", "data", "raw")
+
+for spider in spiders:
+    output_filename = spider + "_" + generate_id() + ".json"
+    output_file = os.path.join(output_base_path, output_filename)
     os.system(f"scrapy crawl {spider} -o {output_file}")
